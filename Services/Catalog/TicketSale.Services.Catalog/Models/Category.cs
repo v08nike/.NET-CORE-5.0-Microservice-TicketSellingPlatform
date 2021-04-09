548da8c123662ffb7cfc384791535873de43d170
v08nike <teddan817@gmail.com>
2021-04-09 23:38:41 +0300

    Add Catalog Model Files.

---
+++ Services/Catalog/TicketSale.Services.Catalog/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TicketSale.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
