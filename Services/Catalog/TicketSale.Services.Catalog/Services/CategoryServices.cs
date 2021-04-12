b75b72791b5fac20b90e4cb031d1922c12344690
v08nike <teddan817@gmail.com>
2021-04-12 12:14:13 +0300

    Add Services and Update Response.

---
+++ Services/Catalog/TicketSale.Services.Catalog/Services/CategoryServices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketSale.Services.Catalog.Models;
+using MongoDB.Driver;
+using AutoMapper;
+using TicketSale.Services.Catalog.Settings;
+using TicketSale.Shared.Dtos;
+using TicketSale.Services.Catalog.Dtos;
+
+namespace TicketSale.Services.Catalog.Services
+{
+    public class CategoryServices
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        private readonly IMapper _mapper;
+
+        internal CategoryServices(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CategoryDto>>> GetAllAsync()
+        {
+            var categories = await _categoryCollection.Find(category => true).ToListAsync();
+
+            return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
+
+        }
+
+        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        {
+            await _categoryCollection.InsertOneAsync(category );
+
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+
+        }
+
+    }
+}
---
+++ Shared/TicketSale.Shared/Dtos/Response.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TicketSale.Shared.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; set; }
+
+        public List<string> Errors { get; private set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static Response<T> Fail(string error, int statusCode)
+        {
+            return new Response<T> { Errors = new List<string>() { error}, StatusCode = statusCode, IsSuccessful = false };
+        }
+    }
+}
