330ec817b6796d76715c726f4f0fc39731088f74
v08nike <teddan817@gmail.com>
2021-05-01 13:24:54 +0300

    Update UserController.

--- IdentityServer/TicketSale.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/TicketSale.IdentityServer/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using TicketSale.IdentityServer.Dto;
@@ -44,5 +45,20 @@ namespace TicketSale.IdentityServer.Controllers
 
             return NoContent();
         }
+      
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City=user.City });
+        }
+    
     }
 }
