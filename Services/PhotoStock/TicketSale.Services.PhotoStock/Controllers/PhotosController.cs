8a2948468a52d6a2ed8c191676368ddd0ecb532e
v08nike <teddan817@gmail.com>
2021-05-01 15:16:41 +0300

    Adding PhotoStoch.API.

---
+++ Services/PhotoStock/TicketSale.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketSale.Services.PhotoStock.Dto;
+using TicketSale.Shared.ControllerBases;
+using TicketSale.Shared.Dtos;
+
+namespace TicketSale.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult>PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream,cancellationToken);
+
+                    var returnPath = "photos/" + photo.FileName;
+
+                    PhotoDto photoDto = new()
+                    {
+                        Url = returnPath
+                    };
+                    return CreatActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+                }
+                return CreatActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+            }
+        }
+
+    }
+}
---
+++ Services/PhotoStock/TicketSale.Services.PhotoStock/Dto/PhotoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSale.Services.PhotoStock.Dto
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
