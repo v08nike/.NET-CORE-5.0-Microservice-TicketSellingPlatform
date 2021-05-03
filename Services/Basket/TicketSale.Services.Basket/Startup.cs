e68df2cfbace3af6a4f498d4de52c55c68464939
v08nike <teddan817@gmail.com>
2021-05-03 17:55:59 +0300

    Adding Redis Settings.

--- Services/Basket/TicketSale.Services.Basket/Startup.cs
+++ Services/Basket/TicketSale.Services.Basket/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketSale.Services.Basket.Settings;
 
 namespace TicketSale.Services.Basket
 {
@@ -25,7 +26,7 @@ namespace TicketSale.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
