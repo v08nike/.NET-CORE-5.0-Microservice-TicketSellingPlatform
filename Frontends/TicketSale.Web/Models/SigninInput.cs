b34b97bb8419b2dc2542994acb53f0867ed4a0c7
v08nike <teddan817@gmail.com>
2021-05-04 20:43:04 +0300

    Adding Models to Web API.

---
+++ Frontends/TicketSale.Web/Models/SigninInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSale.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Email adresiniz")]
+        public string Emial { get; set; }
+
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    
+    }
+}
---
+++ Frontends/TicketSale.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,21 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketSale.Shared.Dtos;
+using TicketSale.Web.Models;
+
+namespace TicketSale.Web.Services.Interfaces
+{
+    interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput) ;
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+
+
+    }
+}
