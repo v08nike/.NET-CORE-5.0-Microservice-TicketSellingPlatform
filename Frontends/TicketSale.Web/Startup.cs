d4c500d3d4703e5a89690aa6fb83d92967a6cb33
v08nike <teddan817@gmail.com>
2021-05-19 17:37:25 +0300

    Adding Service Settings.

--- Frontends/TicketSale.Web/Startup.cs
+++ Frontends/TicketSale.Web/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketSale.Web.Models;
 
 namespace TicketSale.Web
 {
@@ -22,6 +23,7 @@ namespace TicketSale.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.AddControllersWithViews();
         }
 
